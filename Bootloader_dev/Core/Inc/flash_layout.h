00b23f4d2dbb9a863a84a5c8ddfa784b02a0a9d7
Jayeshraudal <jayeshraudal95@gmail.com>
2026-05-12 20:25:40 +0200

    updated flash layout header file

--- Bootloader_dev/Core/Inc/flash_layout.h
+++ Bootloader_dev/Core/Inc/flash_layout.h
@@ -1,3 +1,17 @@
-# define BL_ADDR 0x08000000;        // 32 KB
-# define APP_HEAD_ADDR 0x08008000;  // 16 KB
-# define APP_ADDR 0x0800C000;       // 464 KB
+/*
+ * 		flash_layout.h
+ * 		created on 12.05.2026
+ * 		Author: Jayesh Raundal
+ *
+ *
+ * */
+
+#ifndef INC_FLASH_LAYOUT_H_
+#define NC_FLASH_LAYOUT_H_
+
+# define BL_ADDR                 0x08000000      // 32 KB
+# define APP_HEAD_ADDR           0x08008000  // 16 KB
+# define APP_ADDR                0x0800C000       // 464 KB
+
+
+#endif
