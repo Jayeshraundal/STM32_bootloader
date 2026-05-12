cc2dc152bd236dcd56e852473791982f2ce43496
Jayeshraudal <jayeshraudal95@gmail.com>
2026-05-12 20:44:21 +0200

    fix

--- Bootloader_dev/Core/Inc/flash_layout.h
+++ Bootloader_dev/Core/Inc/flash_layout.h
@@ -10,8 +10,8 @@
 #define INC_FLASH_LAYOUT_H_
 
 # define BL_ADDR                 0x08000000      // 32 KB
-# define APP_HEAD_ADDR           0x08008000  // 16 KB
-# define APP_ADDR                0x0800C000       // 464 KB
+# define APP_HEAD_ADDR           0x08008000      // 16 KB
+# define APP_ADDR                0x0800C000      // 464 KB
 
 
 #endif
